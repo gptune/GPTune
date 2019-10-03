fcc6e06152018d5f84cf4b06dfccc1fb0a96b231
liuyang <liuyangzhuan@lbl.gov>
2019-10-03 16:41:25 -0700

    added runscript on cori

---
+++ config_cori.sh
@@ -0,0 +1,19 @@
+module load python3/3.7-anaconda-2019.07
+module unload cray-mpich/7.7.6
+module use /global/common/software/m3169/cori/modulefiles
+module load openmpi/4.0.1
+
+env CC=mpicc pip install --upgrade --user -r requirements.txt
+make CC=mpicc
+
+ git clone https://github.com/ytopt-team/autotune.git
+ cd autotune/
+ pip install /global/homes/l/liuyangz/Cori/my_research/GPTune/autotune/
+ 
+ git clone https://github.com/scikit-optimize/scikit-optimize.git
+ cd scikit-optimize/
+ pip install /global/homes/l/liuyangz/Cori/my_research/GPTune/scikit-optimize/
+
+ export PYTHONPATH=/global/homes/l/liuyangz/Cori/my_research/GPTune/autotune/
+ export PYTHONPATH=$PYTHONPATH:/global/homes/l/liuyangz/Cori/my_research/GPTune/scikit-optimize/
+
