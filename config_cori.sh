0d302477065ba735c0c915b5fc494a6a026bd0fa
liuyang <liuyangzhuan@lbl.gov>
2019-10-03 17:03:58 -0700

    minor change

--- config_cori.sh
+++ config_cori.sh
@@ -6,14 +6,19 @@ module load openmpi/4.0.1
 env CC=mpicc pip install --upgrade --user -r requirements.txt
 make CC=mpicc
 
+
+ 
  git clone https://github.com/ytopt-team/autotune.git
  cd autotune/
- pip install /global/homes/l/liuyangz/Cori/my_research/GPTune/autotune/
- 
+# pip install /global/homes/l/liuyangz/Cori/my_research/GPTune/autotune/
+ pip install --user -e .
+
  git clone https://github.com/scikit-optimize/scikit-optimize.git
  cd scikit-optimize/
- pip install /global/homes/l/liuyangz/Cori/my_research/GPTune/scikit-optimize/
+# pip install /global/homes/l/liuyangz/Cori/my_research/GPTune/scikit-optimize/
+ pip install --user -e .
+
 
  export PYTHONPATH=/global/homes/l/liuyangz/Cori/my_research/GPTune/autotune/
  export PYTHONPATH=$PYTHONPATH:/global/homes/l/liuyangz/Cori/my_research/GPTune/scikit-optimize/
-
+ python ./demo.py
