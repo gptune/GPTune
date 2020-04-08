33111fb1c477e6482323a337bc99bc39ca118aba
liuyang <liuyangzhuan@lbl.gov>
2020-04-08 11:41:00 -0700

    added cori_knl scripts

---
+++ config_cori_knl.sh
@@ -0,0 +1,118 @@
+module unload darshan
+module swap craype-haswell craype-mic-knl
+module load craype-hugepages2M
+module unload cray-libsci
+
+module load python/3.7-anaconda-2019.07
+module unload cray-mpich/7.7.6
+
+module swap PrgEnv-intel PrgEnv-gnu
+export MKLROOT=/opt/intel/compilers_and_libraries_2018.1.163/linux/mkl
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/intel/compilers_and_libraries_2018.1.163/linux/mkl/lib/intel64
+module load openmpi/4.0.1
+
+
+
+export PYTHONPATH=$PYTHONPATH:$PWD/autotune/
+export PYTHONPATH=$PYTHONPATH:$PWD/scikit-optimize/
+export PYTHONPATH=$PYTHONPATH:$PWD/mpi4py/
+export PYTHONPATH=$PYTHONPATH:$PWD/GPTune/
+export PYTHONWARNINGS=ignore
+
+CCC=mpicc
+CCCPP=mpicxx
+FTN=mpif90
+
+#pip uninstall -r requirements.txt
+#env CC=$CCC pip install --upgrade --user -r requirements.txt
+env CC=$CCC pip install --user -r requirements.txt
+
+
+mkdir -p build
+cd build
+export CRAYPE_LINK_TYPE=dynamic
+rm -rf CMakeCache.txt
+rm -rf DartConfiguration.tcl
+rm -rf CTestTestfile.cmake
+rm -rf cmake_install.cmake
+rm -rf CMakeFiles
+cmake .. \
+	-DCMAKE_CXX_FLAGS="" \
+	-DCMAKE_C_FLAGS="" \
+	-DBUILD_SHARED_LIBS=ON \
+	-DCMAKE_CXX_COMPILER=$CCCPP \
+	-DCMAKE_C_COMPILER=$CCC \
+	-DCMAKE_Fortran_COMPILER=$FTN \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
+	-DTPL_BLAS_LIBRARIES="${MKLROOT}/lib/intel64/libmkl_gf_lp64.so;${MKLROOT}/lib/intel64/libmkl_gnu_thread.so;${MKLROOT}/lib/intel64/libmkl_core.so;${MKLROOT}/lib/intel64/libmkl_def.so;${MKLROOT}/lib/intel64/libmkl_avx.so" \
+	-DTPL_LAPACK_LIBRARIES="${MKLROOT}/lib/intel64/libmkl_gf_lp64.so;${MKLROOT}/lib/intel64/libmkl_gnu_thread.so;${MKLROOT}/lib/intel64/libmkl_core.so;${MKLROOT}/lib/intel64/libmkl_def.so;${MKLROOT}/lib/intel64/libmkl_avx.so" \
+	-DTPL_SCALAPACK_LIBRARIES="${MKLROOT}/lib/intel64/libmkl_blacs_openmpi_lp64.so;${MKLROOT}/lib/intel64/libmkl_scalapack_lp64.so"
+make
+cp lib_gptuneclcm.so ../.
+cp pdqrdriver ../
+
+
+
+cd ../examples/
+git clone https://github.com/xiaoyeli/superlu_dist.git
+cd superlu_dist
+
+wget http://glaros.dtc.umn.edu/gkhome/fetch/sw/parmetis/parmetis-4.0.3.tar.gz
+tar -xf parmetis-4.0.3.tar.gz
+cd parmetis-4.0.3/
+mkdir -p install
+make config shared=1 cc=$CCC cxx=$CCCPP prefix=$PWD/install
+make install > make_parmetis_install.log 2>&1
+
+cd ../
+PARMETIS_INCLUDE_DIRS="$PWD/parmetis-4.0.3/metis/include;$PWD/parmetis-4.0.3/install/include"
+PARMETIS_LIBRARIES=$PWD/parmetis-4.0.3/install/lib/libparmetis.so
+mkdir -p build
+cd build
+rm -rf CMakeCache.txt
+rm -rf DartConfiguration.tcl
+rm -rf CTestTestfile.cmake
+rm -rf cmake_install.cmake
+rm -rf CMakeFiles
+cmake .. \
+	-DCMAKE_CXX_FLAGS="-Ofast -std=c++11 -DAdd_ -DRELEASE" \
+	-DCMAKE_C_FLAGS="-std=c11 -DPRNTlevel=0 -DPROFlevel=0 -DDEBUGlevel=0" \
+	-DBUILD_SHARED_LIBS=OFF \
+	-DCMAKE_CXX_COMPILER=$CCCPP \
+	-DCMAKE_C_COMPILER=$CCC \
+	-DCMAKE_Fortran_COMPILER=$FTN \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
+	-DTPL_BLAS_LIBRARIES="${MKLROOT}/lib/intel64/libmkl_gf_lp64.so;${MKLROOT}/lib/intel64/libmkl_gnu_thread.so;${MKLROOT}/lib/intel64/libmkl_core.so;${MKLROOT}/lib/intel64/libmkl_def.so;${MKLROOT}/lib/intel64/libmkl_avx.so" \
+	-DTPL_LAPACK_LIBRARIES="${MKLROOT}/lib/intel64/libmkl_gf_lp64.so;${MKLROOT}/lib/intel64/libmkl_gnu_thread.so;${MKLROOT}/lib/intel64/libmkl_core.so;${MKLROOT}/lib/intel64/libmkl_def.so;${MKLROOT}/lib/intel64/libmkl_avx.so" \
+	-DTPL_PARMETIS_INCLUDE_DIRS=$PARMETIS_INCLUDE_DIRS \
+	-DTPL_PARMETIS_LIBRARIES=$PARMETIS_LIBRARIES
+make pddrive_spawn
+make pzdrive_spawn
+
+
+
+# make CC=$CCC
+cd ../../../
+rm -rf mpi4py
+git clone https://github.com/mpi4py/mpi4py.git
+cd mpi4py/
+python setup.py build --mpicc="$CCC -shared"
+python setup.py install --user
+# env CC=mpicc pip install --user -e .								  
+
+
+
+# cd ../
+# rm -rf scikit-optimize
+# git clone https://github.com/scikit-optimize/scikit-optimize.git
+# cd scikit-optimize/
+# env CC=$CCC pip install --user -e .
+
+
+cd ../
+rm -rf autotune
+git clone https://github.com/ytopt-team/autotune.git
+cd autotune/
+env CC=$CCC pip install --user -e .
