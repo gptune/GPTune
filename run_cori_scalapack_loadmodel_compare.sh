8af2d3b527fb5a5af927cd9a1e90cb83a5406fe5
Younghyun Cho <yhcho614@gmail.com>
2020-12-09 00:53:33 -0800

    Scripts for MLA_LoadModel evaluation for scalapack and hypre on Cori

--- run_cori_scalapack_loadmodel_compare.sh
+++ run_cori_scalapack_loadmodel_compare.sh
@@ -27,42 +27,48 @@ FTN=mpif90
 
 cd examples
 
-mkdir loadmodel_eval
+mkdir scalapack_loadmodel_eval
 
 ntask=5
+nrun=100
+nodes=8
+cores=32
+nprocmin_pernode=1
+
+mpirun --oversubscribe --mca pmix_server_max_wait 3600 --mca pmix_base_exchange_timeout 3600 --mca orte_abort_timeout 3600 --mca plm_rsh_no_tree_spawn true -n 1 python ./scalapack_MLA_history_db.py -nodes ${nodes} -cores ${cores} -nprocmin_pernode ${nprocmin_pernode} -ntask ${ntask} -nrun ${nrun} -machine cori | tee scalapack_loadmodel_eval/a.out_scalapck_MLA_nodes${nodes}_core${cores}_ntask${ntask}_nrun${nrun}_oversubscribe
+cp scalapack-pdqrdriver.json scalapack_loadmodel_eval/scalapack-pdqrdriver.first.json
+cp modeling_stat_scalapack.csv scalapack_loadmodel_eval/modeling_stat_scalapack.csv
+
+rm scalapack-pdqrdriver.json
+rm modeling_stat_scalapack.csv
+cp scalapack_loadmodel_eval/scalapack-pdqrdriver.first.json scalapack-pdqrdriver.json
+
+nrun=100
+mpirun --oversubscribe --mca pmix_server_max_wait 3600 --mca pmix_base_exchange_timeout 3600 --mca orte_abort_timeout 3600 --mca plm_rsh_no_tree_spawn true -n 1 python ./scalapack_MLA_history_db.py -nodes ${nodes} -cores ${cores} -nprocmin_pernode ${nprocmin_pernode} -ntask ${ntask} -nrun ${nrun} -machine cori | tee scalapack_loadmodel_eval/a.out_scalapck_MLA_nodes${nodes}_core${cores}_ntask${ntask}_nrun${nrun}_oversubscribe_history_db
+cp scalapack-pdqrdriver.json scalapack_loadmodel_eval/scalapack-pdqrdriver.history_db.json
+cp modeling_stat_scalapack.csv scalapack_loadmodel_eval/modeling_stat_scalapack.history_db.csv
+
+rm scalapack-pdqrdriver.json
+rm modeling_stat_scalapack.csv
+cp scalapack_loadmodel_eval/scalapack-pdqrdriver.first.json scalapack-pdqrdriver.json
+
+nrun=100
+mpirun --oversubscribe --mca pmix_server_max_wait 3600 --mca pmix_base_exchange_timeout 3600 --mca orte_abort_timeout 3600 --mca plm_rsh_no_tree_spawn true -n 1 python ./scalapack_MLA_loadmodel.py -nodes ${nodes} -cores ${cores} -nprocmin_pernode ${nprocmin_pernode} -ntask ${ntask} -nrun ${nrun} -machine cori | tee scalapack_loadmodel_eval/a.out_scalapck_MLA_nodes${nodes}_core${cores}_ntask${ntask}_nrun${nrun}_oversubscribe_loadmodel_max_eval
+cp scalapack-pdqrdriver.json scalapack_loadmodel_eval/scalapack-pdqrdriver.loadmodel.json
+cp modeling_stat_scalapack.csv scalapack_loadmodel_eval/modeling_stat_scalapack.loadmodel.csv
+
+rm scalapack-pdqrdriver.json
+rm modeling_stat_scalapack.csv
+
 nrun=200
-nodes=8
-cores=32
-nprocmin_pernode=1
-
-mpirun --oversubscribe --mca pmix_server_max_wait 3600 --mca pmix_base_exchange_timeout 3600 --mca orte_abort_timeout 3600 --mca plm_rsh_no_tree_spawn true -n 1 python ./scalapack_MLA_history_db.py -nodes ${nodes} -cores ${cores} -nprocmin_pernode ${nprocmin_pernode} -ntask ${ntask} -nrun ${nrun} -machine cori | tee loadmodel_eval/a.out_scalapck_MLA_nodes${nodes}_core${cores}_ntask${ntask}_nrun${nrun}_oversubscribe
-cp scalapack-pdqrdriver.json loadmodel_eval/scalapack-pdqrdriver.first.json
-cp modeling_stat_scalapack.csv loadmodel_eval/modeling_stat_scalapack.csv
+mpirun --oversubscribe --mca pmix_server_max_wait 3600 --mca pmix_base_exchange_timeout 3600 --mca orte_abort_timeout 3600 --mca plm_rsh_no_tree_spawn true -n 1 python ./scalapack_MLA_history_db.py -nodes ${nodes} -cores ${cores} -nprocmin_pernode ${nprocmin_pernode} -ntask ${ntask} -nrun ${nrun} -machine cori | tee scalapack_loadmodel_eval/a.out_scalapck_MLA_nodes${nodes}_core${cores}_ntask${ntask}_nrun${nrun}_oversubscribe_singlerun
+cp scalapack-pdqrdriver.json scalapack_loadmodel_eval/scalapack-pdqrdriver.singlerun.json
+cp modeling_stat_scalapack.csv scalapack_loadmodel_eval/modeling_stat_scalapack.singlerun.csv
 
 rm scalapack-pdqrdriver.json
 rm modeling_stat_scalapack.csv
-cp loadmodel_eval/scalapack-pdqrdriver.first.json scalapack-pdqrdriver.json
 
-nrun=50
-mpirun --oversubscribe --mca pmix_server_max_wait 3600 --mca pmix_base_exchange_timeout 3600 --mca orte_abort_timeout 3600 --mca plm_rsh_no_tree_spawn true -n 1 python ./scalapack_MLA_history_db.py -nodes ${nodes} -cores ${cores} -nprocmin_pernode ${nprocmin_pernode} -ntask ${ntask} -nrun ${nrun} -machine cori | tee loadmodel_eval/a.out_scalapck_MLA_nodes${nodes}_core${cores}_ntask${ntask}_nrun${nrun}_oversubscribe_history_db
-cp scalapack-pdqrdriver.json loadmodel_eval/scalapack-pdqrdriver.history_db.json
-cp modeling_stat_scalapack.csv loadmodel_eval/modeling_stat_scalapack.history_db.csv
-
-rm scalapack-pdqrdriver.json
-rm modeling_stat_scalapack.csv
-cp loadmodel_eval/scalapack-pdqrdriver.first.json scalapack-pdqrdriver.json
-
-nrun=50
-mpirun --oversubscribe --mca pmix_server_max_wait 3600 --mca pmix_base_exchange_timeout 3600 --mca orte_abort_timeout 3600 --mca plm_rsh_no_tree_spawn true -n 1 python ./scalapack_MLA_loadmodel.py -nodes ${nodes} -cores ${cores} -nprocmin_pernode ${nprocmin_pernode} -ntask ${ntask} -nrun ${nrun} -machine cori | tee loadmodel_eval/a.out_scalapck_MLA_nodes${nodes}_core${cores}_ntask${ntask}_nrun${nrun}_oversubscribe_loadmodel_max_eval
-cp scalapack-pdqrdriver.json loadmodel_eval/scalapack-pdqrdriver.loadmodel.json
-cp modeling_stat_scalapack.csv loadmodel_eval/modeling_stat_scalapack.loadmodel.csv
-
-rm scalapack-pdqrdriver.json
-rm modeling_stat_scalapack.csv
-cp loadmodel_eval/scalapack-pdqrdriver.first.json scalapack-pdqrdriver.json
-
-
-mkdir loadmodel_eval2
+mkdir scalapack_loadmodel_eval2
 
 ntask=5
 nrun=50
@@ -70,38 +76,47 @@ nodes=8
 cores=32
 nprocmin_pernode=1
 
-mpirun --oversubscribe --mca pmix_server_max_wait 3600 --mca pmix_base_exchange_timeout 3600 --mca orte_abort_timeout 3600 --mca plm_rsh_no_tree_spawn true -n 1 python ./scalapack_MLA_history_db.py -nodes ${nodes} -cores ${cores} -nprocmin_pernode ${nprocmin_pernode} -ntask ${ntask} -nrun ${nrun} -machine cori | tee loadmodel_eval2/a.out_scalapck_MLA_nodes${nodes}_core${cores}_ntask${ntask}_nrun${nrun}_oversubscribe
+mpirun --oversubscribe --mca pmix_server_max_wait 3600 --mca pmix_base_exchange_timeout 3600 --mca orte_abort_timeout 3600 --mca plm_rsh_no_tree_spawn true -n 1 python ./scalapack_MLA_history_db.py -nodes ${nodes} -cores ${cores} -nprocmin_pernode ${nprocmin_pernode} -ntask ${ntask} -nrun ${nrun} -machine cori | tee scalapack_loadmodel_eval2/a.out_scalapck_MLA_nodes${nodes}_core${cores}_ntask${ntask}_nrun${nrun}_oversubscribe
 
-cp scalapack-pdqrdriver.json loadmodel_eval2/scalapack-pdqrdriver.first.json
-cp modeling_stat_scalapack.csv loadmodel_eval2/modeling_stat_scalapack.csv
+cp scalapack-pdqrdriver.json scalapack_loadmodel_eval2/scalapack-pdqrdriver.first.json
+cp modeling_stat_scalapack.csv scalapack_loadmodel_eval2/modeling_stat_scalapack.csv
 
 rm scalapack-pdqrdriver.json
 rm modeling_stat_scalapack.csv
 
-cp loadmodel_eval2/scalapack-pdqrdriver.first.json scalapack-pdqrdriver.json
+cp scalapack_loadmodel_eval2/scalapack-pdqrdriver.first.json scalapack-pdqrdriver.json
 
 nrun=50
-mpirun --oversubscribe --mca pmix_server_max_wait 3600 --mca pmix_base_exchange_timeout 3600 --mca orte_abort_timeout 3600 --mca plm_rsh_no_tree_spawn true -n 1 python ./scalapack_MLA_history_db.py -nodes ${nodes} -cores ${cores} -nprocmin_pernode ${nprocmin_pernode} -ntask ${ntask} -nrun ${nrun} -machine cori | tee loadmodel_eval2/a.out_scalapck_MLA_nodes${nodes}_core${cores}_ntask${ntask}_nrun${nrun}_oversubscribe_history_db
+mpirun --oversubscribe --mca pmix_server_max_wait 3600 --mca pmix_base_exchange_timeout 3600 --mca orte_abort_timeout 3600 --mca plm_rsh_no_tree_spawn true -n 1 python ./scalapack_MLA_history_db.py -nodes ${nodes} -cores ${cores} -nprocmin_pernode ${nprocmin_pernode} -ntask ${ntask} -nrun ${nrun} -machine cori | tee scalapack_loadmodel_eval2/a.out_scalapck_MLA_nodes${nodes}_core${cores}_ntask${ntask}_nrun${nrun}_oversubscribe_history_db
 
-cp scalapack-pdqrdriver.json loadmodel_eval2/scalapack-pdqrdriver.history_db.json
-cp modeling_stat_scalapack.csv loadmodel_eval2/modeling_stat_scalapack.history_db.csv
+cp scalapack-pdqrdriver.json scalapack_loadmodel_eval2/scalapack-pdqrdriver.history_db.json
+cp modeling_stat_scalapack.csv scalapack_loadmodel_eval2/modeling_stat_scalapack.history_db.csv
 
 rm scalapack-pdqrdriver.json
 rm modeling_stat_scalapack.csv
 
-cp loadmodel_eval2/scalapack-pdqrdriver.first.json scalapack-pdqrdriver.json
+cp scalapack_loadmodel_eval2/scalapack-pdqrdriver.first.json scalapack-pdqrdriver.json
 
 nrun=50
-mpirun --oversubscribe --mca pmix_server_max_wait 3600 --mca pmix_base_exchange_timeout 3600 --mca orte_abort_timeout 3600 --mca plm_rsh_no_tree_spawn true -n 1 python ./scalapack_MLA_loadmodel.py -nodes ${nodes} -cores ${cores} -nprocmin_pernode ${nprocmin_pernode} -ntask ${ntask} -nrun ${nrun} -machine cori | tee loadmodel_eval2/a.out_scalapck_MLA_nodes${nodes}_core${cores}_ntask${ntask}_nrun${nrun}_oversubscribe_loadmodel_max_eval
+mpirun --oversubscribe --mca pmix_server_max_wait 3600 --mca pmix_base_exchange_timeout 3600 --mca orte_abort_timeout 3600 --mca plm_rsh_no_tree_spawn true -n 1 python ./scalapack_MLA_loadmodel.py -nodes ${nodes} -cores ${cores} -nprocmin_pernode ${nprocmin_pernode} -ntask ${ntask} -nrun ${nrun} -machine cori | tee scalapack_loadmodel_eval2/a.out_scalapck_MLA_nodes${nodes}_core${cores}_ntask${ntask}_nrun${nrun}_oversubscribe_loadmodel_max_eval
 
-cp scalapack-pdqrdriver.json loadmodel_eval2/scalapack-pdqrdriver.loadmodel.json
-cp modeling_stat_scalapack.csv loadmodel_eval2/modeling_stat_scalapack.loadmodel.csv
+cp scalapack-pdqrdriver.json scalapack_loadmodel_eval2/scalapack-pdqrdriver.loadmodel.json
+cp modeling_stat_scalapack.csv scalapack_loadmodel_eval2/modeling_stat_scalapack.loadmodel.csv
+
+rm scalapack-pdqrdriver.json
+rm modeling_stat_scalapack.csv
+
+nrun=100
+mpirun --oversubscribe --mca pmix_server_max_wait 3600 --mca pmix_base_exchange_timeout 3600 --mca orte_abort_timeout 3600 --mca plm_rsh_no_tree_spawn true -n 1 python ./scalapack_MLA_history_db.py -nodes ${nodes} -cores ${cores} -nprocmin_pernode ${nprocmin_pernode} -ntask ${ntask} -nrun ${nrun} -machine cori | tee scalapack_loadmodel_eval2/a.out_scalapck_MLA_nodes${nodes}_core${cores}_ntask${ntask}_nrun${nrun}_oversubscribe_singlerun
+
+cp scalapack-pdqrdriver.json scalapack_loadmodel_eval2/scalapack-pdqrdriver.singlerun.json
+cp modeling_stat_scalapack.csv scalapack_loadmodel_eval2/modeling_stat_scalapack.singlerun.csv
 
 rm scalapack-pdqrdriver.json
 rm modeling_stat_scalapack.csv
 
 
-mkdir loadmodel_eval3
+mkdir scalapack_loadmodel_eval3
 
 ntask=5
 nrun=20
@@ -109,33 +124,41 @@ nodes=8
 cores=32
 nprocmin_pernode=1
 
-mpirun --oversubscribe --mca pmix_server_max_wait 3600 --mca pmix_base_exchange_timeout 3600 --mca orte_abort_timeout 3600 --mca plm_rsh_no_tree_spawn true -n 1 python ./scalapack_MLA_history_db.py -nodes ${nodes} -cores ${cores} -nprocmin_pernode ${nprocmin_pernode} -ntask ${ntask} -nrun ${nrun} -machine cori | tee loadmodel_eval3/a.out_scalapck_MLA_nodes${nodes}_core${cores}_ntask${ntask}_nrun${nrun}_oversubscribe
+mpirun --oversubscribe --mca pmix_server_max_wait 3600 --mca pmix_base_exchange_timeout 3600 --mca orte_abort_timeout 3600 --mca plm_rsh_no_tree_spawn true -n 1 python ./scalapack_MLA_history_db.py -nodes ${nodes} -cores ${cores} -nprocmin_pernode ${nprocmin_pernode} -ntask ${ntask} -nrun ${nrun} -machine cori | tee scalapack_loadmodel_eval3/a.out_scalapck_MLA_nodes${nodes}_core${cores}_ntask${ntask}_nrun${nrun}_oversubscribe
 
-cp scalapack-pdqrdriver.json loadmodel_eval3/scalapack-pdqrdriver.first.json
-cp modeling_stat_scalapack.csv loadmodel_eval3/modeling_stat_scalapack.csv
+cp scalapack-pdqrdriver.json scalapack_loadmodel_eval3/scalapack-pdqrdriver.first.json
+cp modeling_stat_scalapack.csv scalapack_loadmodel_eval3/modeling_stat_scalapack.csv
 
 rm scalapack-pdqrdriver.json
 rm modeling_stat_scalapack.csv
 
-cp loadmodel_eval3/scalapack-pdqrdriver.first.json scalapack-pdqrdriver.json
+cp scalapack_loadmodel_eval3/scalapack-pdqrdriver.first.json scalapack-pdqrdriver.json
 
 nrun=20
-mpirun --oversubscribe --mca pmix_server_max_wait 3600 --mca pmix_base_exchange_timeout 3600 --mca orte_abort_timeout 3600 --mca plm_rsh_no_tree_spawn true -n 1 python ./scalapack_MLA_history_db.py -nodes ${nodes} -cores ${cores} -nprocmin_pernode ${nprocmin_pernode} -ntask ${ntask} -nrun ${nrun} -machine cori | tee loadmodel_eval3/a.out_scalapck_MLA_nodes${nodes}_core${cores}_ntask${ntask}_nrun${nrun}_oversubscribe_history_db
+mpirun --oversubscribe --mca pmix_server_max_wait 3600 --mca pmix_base_exchange_timeout 3600 --mca orte_abort_timeout 3600 --mca plm_rsh_no_tree_spawn true -n 1 python ./scalapack_MLA_history_db.py -nodes ${nodes} -cores ${cores} -nprocmin_pernode ${nprocmin_pernode} -ntask ${ntask} -nrun ${nrun} -machine cori | tee scalapack_loadmodel_eval3/a.out_scalapck_MLA_nodes${nodes}_core${cores}_ntask${ntask}_nrun${nrun}_oversubscribe_history_db
 
-cp scalapack-pdqrdriver.json loadmodel_eval3/scalapack-pdqrdriver.history_db.json
-cp modeling_stat_scalapack.csv loadmodel_eval3/modeling_stat_scalapack.history_db.csv
+cp scalapack-pdqrdriver.json scalapack_loadmodel_eval3/scalapack-pdqrdriver.history_db.json
+cp modeling_stat_scalapack.csv scalapack_loadmodel_eval3/modeling_stat_scalapack.history_db.csv
 
 rm scalapack-pdqrdriver.json
 rm modeling_stat_scalapack.csv
 
-cp loadmodel_eval3/scalapack-pdqrdriver.first.json scalapack-pdqrdriver.json
+cp scalapack_loadmodel_eval3/scalapack-pdqrdriver.first.json scalapack-pdqrdriver.json
 
 nrun=20
-mpirun --oversubscribe --mca pmix_server_max_wait 3600 --mca pmix_base_exchange_timeout 3600 --mca orte_abort_timeout 3600 --mca plm_rsh_no_tree_spawn true -n 1 python ./scalapack_MLA_loadmodel.py -nodes ${nodes} -cores ${cores} -nprocmin_pernode ${nprocmin_pernode} -ntask ${ntask} -nrun ${nrun} -machine cori | tee loadmodel_eval3/a.out_scalapck_MLA_nodes${nodes}_core${cores}_ntask${ntask}_nrun${nrun}_oversubscribe_loadmodel_max_eval
+mpirun --oversubscribe --mca pmix_server_max_wait 3600 --mca pmix_base_exchange_timeout 3600 --mca orte_abort_timeout 3600 --mca plm_rsh_no_tree_spawn true -n 1 python ./scalapack_MLA_loadmodel.py -nodes ${nodes} -cores ${cores} -nprocmin_pernode ${nprocmin_pernode} -ntask ${ntask} -nrun ${nrun} -machine cori | tee scalapack_loadmodel_eval3/a.out_scalapck_MLA_nodes${nodes}_core${cores}_ntask${ntask}_nrun${nrun}_oversubscribe_loadmodel_max_eval
 
-cp scalapack-pdqrdriver.json loadmodel_eval3/scalapack-pdqrdriver.loadmodel.json
-cp modeling_stat_scalapack.csv loadmodel_eval3/modeling_stat_scalapack.loadmodel.csv
+cp scalapack-pdqrdriver.json scalapack_loadmodel_eval3/scalapack-pdqrdriver.loadmodel.json
+cp modeling_stat_scalapack.csv scalapack_loadmodel_eval3/modeling_stat_scalapack.loadmodel.csv
 
 rm scalapack-pdqrdriver.json
 rm modeling_stat_scalapack.csv
 
+nrun=40
+mpirun --oversubscribe --mca pmix_server_max_wait 3600 --mca pmix_base_exchange_timeout 3600 --mca orte_abort_timeout 3600 --mca plm_rsh_no_tree_spawn true -n 1 python ./scalapack_MLA_history_db.py -nodes ${nodes} -cores ${cores} -nprocmin_pernode ${nprocmin_pernode} -ntask ${ntask} -nrun ${nrun} -machine cori | tee scalapack_loadmodel_eval3/a.out_scalapck_MLA_nodes${nodes}_core${cores}_ntask${ntask}_nrun${nrun}_oversubscribe_singlerun
+
+cp scalapack-pdqrdriver.json scalapack_loadmodel_eval3/scalapack-pdqrdriver.singlerun.json
+cp modeling_stat_scalapack.csv scalapack_loadmodel_eval3/modeling_stat_scalapack.singlerun.csv
+
+rm scalapack-pdqrdriver.json
+rm modeling_stat_scalapack.csv
