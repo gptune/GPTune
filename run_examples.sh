30888677bc335d4cb265f40404da6851f80d51be
liuyangzhuan <liuyangzhuan@gmail.com>
2021-04-07 23:47:11 -0700

    Update run_examples.sh

--- run_examples.sh
+++ run_examples.sh
@@ -52,10 +52,10 @@ elif [[ $(uname -s) = "Darwin" ]]; then
     export machine=mac
 elif [[ $(dnsdomainname) = "summit.olcf.ornl.gov" ]]; then
     export machine=summit
+elif [[ $(cat /etc/os-release | grep "PRETTY_NAME") == *"Ubuntu"* || $(cat /etc/os-release | grep "PRETTY_NAME") == *"Debian"* ]]; then
+    export machine=cleanlinux    
 elif [[ $(hostname -s) = "tr4-workstation" ]]; then
     export machine=tr4-workstation
-elif [[ $(cat /etc/os-release | grep "PRETTY_NAME") == *"Ubuntu"* || $(cat /etc/os-release | grep "PRETTY_NAME") == *"Debian"* ]]; then
-    export machine=cleanlinux
 fi    
 
 
@@ -328,7 +328,7 @@ if [[ $ModuleEnv == *"openmpi"* ]]; then
         # tp=STRUMPACK_KRR
         # app_json=$(echo "{\"tuning_problem_name\":\"$tp\"")
         # echo "$app_json$machine_json$software_json$loadable_machine_json$loadable_software_json}" | jq '.' > .gptune/meta.json 
-        # $MPIRUN --oversubscribe --allow-run-as-root --mca pmix_server_max_wait 3600 --mca pmix_base_exchange_timeout 3600 --mca orte_abort_timeout 3600 --mca plm_rsh_no_tree_spawn true -n 1  python ./strumpack_MLA_KRR.py -ntask 1 -nrun 10 -machine cori 
+        # $MPIRUN --oversubscribe --allow-run-as-root --mca pmix_server_max_wait 3600 --mca pmix_base_exchange_timeout 3600 --mca orte_abort_timeout 3600 --mca plm_rsh_no_tree_spawn true -n 1  python ./strumpack_MLA_KRR.py -ntask 1 -nrun 10 -machine cori -npernode $cores 
 
 
 
