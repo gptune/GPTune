9ac49385c010e715ea9f260225045f5ec2f2740f
liuyangzhuan <liuyangzhuan@gmail.com>
2021-04-08 09:24:09 -0700

    Update run_examples.sh

--- run_examples.sh
+++ run_examples.sh
@@ -322,7 +322,6 @@ if [[ $ModuleEnv == *"openmpi"* ]]; then
         # $MPIRUN --oversubscribe --allow-run-as-root --mca pmix_server_max_wait 3600 --mca pmix_base_exchange_timeout 3600 --mca orte_abort_timeout 3600 --mca plm_rsh_no_tree_spawn true -n 1  python ./strumpack_MLA_Poisson3d.py -ntask 1 -nrun 10 -machine cori 
 
 
-        # ###### this one has a segmentation fault when running on Cori, also seems to fail on MacOS
         # cd $GPTUNEROOT/examples/STRUMPACK
         # rm -rf gptune.db/*.json # do not load any database
         # tp=STRUMPACK_KRR
