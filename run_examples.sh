f0b39428c352746c03f096778c3df48765df325e
Yang Liu <liuyangzhuan@gmail.com>
2021-04-26 15:22:35 -0700

    minor update

--- run_examples.sh
+++ run_examples.sh
@@ -572,14 +572,14 @@ if [[ $BuildExample == 1 ]]; then
     echo "$app_json$machine_json$software_json$loadable_machine_json$loadable_software_json}" | jq '.' > .gptune/meta.json
     
     seed=881
-    for expid in TEST1
+    for expid in 1 2 3
     do  
-        seed=$( expr ${seed} + 1 )
+        seed=$( expr ${seed} + ${expid} )
         nstepmax=30
         nstepmin=3
         Nloop=1
         optimization='GPTune'
-        bash nimrod_single_MB_RCI.sh -a $nstepmax -b $nstepmin -c $Nloop -d $optimization -e $expid -f $seed | tee log.nimrod_nstepmax${nstepmax}_nstepmin$nstepmin}_Nloop${Nloop}_optimization${optimization}_nodes${nodes}_expid${expid}_seed${seed} #a: nstepmax b: nstepmin c: Nloop d: optimization e: expid f: seed
+        bash nimrod_single_MB_RCI.sh -a $nstepmax -b $nstepmin -c $Nloop -d $optimization -e $expid -f $seed | tee log.nimrod_nstepmax${nstepmax}_nstepmin${nstepmin}_Nloop${Nloop}_optimization${optimization}_nodes${nodes}_expid${expid}_seed${seed} #a: nstepmax b: nstepmin c: Nloop d: optimization e: expid f: seed
         cp gptune.db/NIMROD.json  gptune.db/NIMROD.json_$(timestamp)
     done
 fi
