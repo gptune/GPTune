525ff0e7152a94d1834b19275b14748761599f94
Younghyun Cho <yhcho614@gmail.com>
2021-07-28 21:08:44 -0700

    Update on newtonsketch tuning

--- examples/NewtonSketch/run_experiment.sh
+++ examples/NewtonSketch/run_experiment.sh
@@ -1,5 +1,12 @@
 #git clone https://github.com/younghyunc/newtonsketch
-#cp .gptune/cori-haswell.sh .gptune/meta.json
+
+cp .gptune/cori-haswell.json .gptune/meta.json
+
+cd ../../
+source env.sh
+cd -
 export PYTHONPATH=$PYTHONPATH:$PWD/newtonsketch
-#mpirun -n 1 python tune_sketch.py -dataset synthetic_high_coherence -nrun 20 -npilot 20 | tee synthetic_high_coherence.a.out
+
+mpirun -n 1 python tune_sketch.py -dataset susy_100Kn -nrun 40 -npilot 20 | tee susy_100Kn.a.out
+mpirun -n 1 python tune_sketch.py -dataset synthetic_high_coherence -nrun 40 -npilot 20 | tee synthetic_high_coherence.a.out
 mpirun -n 1 python tune_sketch.py -dataset cifar-10 -nrun 40 -npilot 20 | tee cifar-10.a.out
