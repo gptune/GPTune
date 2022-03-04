5231d32c675f3e80b44a556eafbd65e2202a6628
Yang Liu <liuyangzhuan@gmail.com>
2022-03-04 13:54:56 -0800

    adding scripts for tuning nimrod with 3d superlu

---
+++ examples/NIMROD_RCI/nimrod_single_slu3d_MB_RCI.py
@@ -0,0 +1,294 @@
+#! /usr/bin/env python
+
+# GPTune Copyright (c) 2019, The Regents of the University of California,
+# through Lawrence Berkeley National Laboratory (subject to receipt of any
+# required approvals from the U.S.Dept. of Energy) and the University of
+# California, Berkeley.  All rights reserved.
+#
+# If you have questions about your rights to use or distribute this software,
+# please contact Berkeley Lab's Intellectual Property Office at IPO@lbl.gov.
+#
+# NOTICE. This Software was developed under funding from the U.S. Department
+# of Energy and the U.S. Government consequently retains certain rights.
+# As such, the U.S. Government has been granted for itself and others acting
+# on its behalf a paid-up, nonexclusive, irrevocable, worldwide license in
+# the Software to reproduce, distribute copies to the public, prepare
+# derivative works, and perform publicly and display publicly, and to permit
+# other to do so.
+#
+
+
+################################################################################
+
+from autotune.search import *
+from autotune.space import *
+from autotune.problem import *
+from gptune import * # import all
+
+import sys
+import os
+# import mpi4py
+# from mpi4py import MPI
+import numpy as np
+import time
+import argparse
+# from callopentuner import OpenTuner
+# from callhpbandster import HpBandSter, HpBandSter_bandit
+import logging
+import math
+
+sys.path.insert(0, os.path.abspath(__file__ + "/../../../GPTune/"))
+logging.getLogger('matplotlib.font_manager').disabled = True
+
+
+################################################################################
+
+
+
+# Define Problem
+def objectives(point):
+    print('objective is not needed when options["RCI_mode"]=True')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+
+    
+    args = parse_args()
+    ntask = args.ntask
+    Nloop = args.Nloop
+    bmin = args.bmin
+    bmax = args.bmax
+    eta = args.eta
+    expid = args.expid
+    TUNER_NAME = args.optimization
+    (machine, processor, nodes, cores) = GetMachineConfiguration()
+    ot.RandomGenerator.SetSeed(args.seed)
+    print(args)
+    print ("machine: " + machine + " processor: " + processor + " num_nodes: " + str(nodes) + " num_cores: " + str(cores))
+
+    nstepmax = args.nstepmax
+    nstepmin = args.nstepmin
+    
+    os.environ['TUNER_NAME'] = TUNER_NAME
+
+
+
+    # Input parameters
+    # ROWPERM   = Categoricalnorm (['1', '2'], transform="onehot", name="ROWPERM")
+    # COLPERM   = Categoricalnorm (['2', '4'], transform="onehot", name="COLPERM")
+    # nprows    = Integer     (0, 5, transform="normalize", name="nprows")
+    # nproc    = Integer     (5, 6, transform="normalize", name="nproc")
+    NSUP      = Integer     (30, 300, transform="normalize", name="NSUP")
+    NREL      = Integer     (10, 40, transform="normalize", name="NREL")
+    nbx      = Integer     (1, 3, transform="normalize", name="nbx")	
+    nby      = Integer     (1, 3, transform="normalize", name="nby")	
+    npz      = Integer     (0, 5, transform="normalize", name="npz")	
+
+    time   = Real        (float("-Inf") , float("Inf"), transform="normalize", name="time")
+
+    # nstep      = Integer     (3, 15, transform="normalize", name="nstep")
+    lphi      = Integer     (1, 3, transform="normalize", name="lphi")
+    mx      = Integer     (5, 6, transform="normalize", name="mx")
+    my      = Integer     (7, 8, transform="normalize", name="my")
+
+    IS = Space([mx,my,lphi])
+    # PS = Space([ROWPERM, COLPERM, nprows, nproc, NSUP, NREL])
+    # PS = Space([ROWPERM, COLPERM, NSUP, NREL, nbx, nby])
+    PS = Space([NSUP, NREL, nbx, nby, npz])
+    OS = Space([time])
+    cst1 = "NSUP >= NREL"
+    constraints = {"cst1" : cst1}
+    models = {}
+    constants={"nodes":nodes,"cores":cores,"nstepmin":nstepmin,"nstepmax":nstepmax,"bmin":bmin,"bmax":bmax,"eta":eta}
+
+    # """ Print all input and parameter samples """	
+    # print(IS, PS, OS, constraints, models)
+
+    # BINDIR = os.path.abspath("/project/projectdirs/m2957/liuyangz/my_research/nimrod/nimdevel_spawn/build_haswell_gnu_openmpi/bin")
+    # RUNDIR = os.path.abspath("/project/projectdirs/m2957/liuyangz/my_research/nimrod/nimrod_input")
+    # os.system("cp %s/nimrod.in ./nimrod_template.in"%(RUNDIR))
+    # os.system("cp %s/fluxgrid.in ."%(RUNDIR))
+    # os.system("cp %s/g163518.03130 ."%(RUNDIR))
+    # os.system("cp %s/p163518.03130 ."%(RUNDIR))
+    # os.system("cp %s/nimset ."%(RUNDIR))
+    # os.system("cp %s/nimrod ./nimrod_spawn"%(BINDIR))
+
+
+
+    problem = TuningProblem(IS, PS, OS, objectives, constraints, None, constants=constants)
+    computer = Computer(nodes = nodes, cores = cores, hosts = None)  
+
+    """ Set and validate options """	
+    options = Options()
+    options['RCI_mode'] = True
+    options['model_restarts'] = 1
+    options['distributed_memory_parallelism'] = False
+    options['shared_memory_parallelism'] = False
+    options['objective_evaluation_parallelism'] = False
+    # options['objective_multisample_threads'] = 1
+    # options['objective_multisample_processes'] = 1
+    # options['objective_nprocmax'] = 1
+    options['model_processes'] = 1
+    # options['model_threads'] = 1
+    # options['model_restart_processes'] = 1
+    # options['search_multitask_processes'] = 1
+    # options['search_multitask_threads'] = 1
+    # options['search_threads'] = 16
+    # options['mpi_comm'] = None
+    # options['mpi_comm'] = mpi4py.MPI.COMM_WORLD
+    options['model_class'] = 'Model_GPy_LCM' # 'Model_LCM'
+    options['verbose'] = False
+    options['sample_class'] = 'SampleOpenTURNS'
+    # options['sample_class'] = 'SampleLHSMDU'
+    # options['sample_algo'] = 'LHS-MDU'
+    options.validate(computer=computer)
+
+    options['budget_min'] = bmin
+    options['budget_max'] = bmax
+    options['budget_base'] = eta
+    smax = int(np.floor(np.log(options['budget_max']/options['budget_min'])/np.log(options['budget_base'])))
+    budgets = [options['budget_max'] /options['budget_base']**x for x in range(smax+1)]
+    NSs = [int((smax+1)/(s+1))*options['budget_base']**s for s in range(smax+1)] 
+    NSs_all = NSs.copy()
+    budget_all = budgets.copy()
+    for s in range(smax+1):
+        for n in range(s):
+            NSs_all.append(int(NSs[s]/options['budget_base']**(n+1)))
+            budget_all.append(int(budgets[s]*options['budget_base']**(n+1)))
+    Ntotal = int(sum(NSs_all) * Nloop)
+    Btotal = int(np.dot(np.array(NSs_all), np.array(budget_all))/options['budget_max']*Nloop) # total number of evaluations at highest budget -- used for single-fidelity tuners
+    print(f"bmin = {bmin}, bmax = {bmax}, eta = {eta}, smax = {smax}")
+    print("samples in one multi-armed bandit loop, NSs_all = ", NSs_all)
+    print("total number of samples: ", Ntotal)
+    print("total number of evaluations at highest budget: ", Btotal)
+    print(f"Sampler: {options['sample_class']}, {options['sample_algo']}")
+    print()
+    
+    data = Data(problem)
+    # giventask = [[1.0], [5.0], [10.0]]
+    # giventask = [[1.0], [1.2], [1.3]]
+    giventask = [[6,8,1]]
+    Pdefault = [128,20,2,2,0]
+    # t_end = args.t_end
+    # giventask = [[i] for i in np.arange(1, t_end, (t_end-1)/ntask).tolist()] # 10 tasks
+    # giventask = [[i] for i in np.arange(1.0, 6.0, 1.0).tolist()] # 5 tasks
+    NI=len(giventask)
+    assert NI == ntask # make sure number of tasks match
+    
+    np.set_printoptions(suppress=False, precision=4)
+    if(TUNER_NAME=='GPTune'):
+        NS = Btotal
+        if args.nrun > 0:
+            NS = args.nrun
+        NS1 = max(NS//2, 1)
+        
+        data.I = giventask
+        data.P = [[Pdefault]] * NI
+
+        gt = GPTune(problem, computer=computer, data=data, options=options, driverabspath=os.path.abspath(__file__))        
+        """ Building MLA with the given list of tasks """
+        (data, model, stats) = gt.MLA(NS=NS, NI=NI, Igiven=giventask, NS1=NS1)
+        # print("stats: ", stats)
+        print("Sampler class: ", options['sample_class'], "Sample algo:", options['sample_algo'])
+        print("Model class: ", options['model_class'])
+        results_file = open(f"nimrod_ntask{args.ntask}_bandit{args.bmin}-{args.bmax}-{args.eta}_Nloop{args.Nloop}_expid{args.expid}.txt", "a")
+        results_file.write(f"Tuner: {TUNER_NAME}\n")
+        results_file.write(f"stats: {stats}\n")        
+        if options['model_class'] == 'Model_LCM' and NI > 1:
+            print("Get correlation metric ... ")
+            C = model[0].M.kern.get_correlation_metric()
+            print("The correlation matrix C is \n", C)
+        elif options['model_class'] == 'Model_GPy_LCM' and NI > 1:
+            print("Get correlation metric ... ")
+            C = model[0].get_correlation_metric(NI)
+            print("The correlation matrix C is \n", C)
+
+        
+        """ Print all input and parameter samples """
+        for tid in range(NI):
+            print("tid: %d"%(tid))
+            print("    mx:%s my:%s lphi:%s"%(data.I[tid][0],data.I[tid][1],data.I[tid][2]))
+            print("    Ps ", data.P[tid])
+            print("    Os ", data.O[tid])
+            print('    Popt ', data.P[tid][np.argmin(data.O[tid])], f'Oopt  {min(data.O[tid])[0]:.3f}', 'nth ', np.argmin(data.O[tid]))
+            results_file.write(f"tid: {tid:d}\n")
+            results_file.write(f"    mx:{data.I[tid][0]:d} my:{data.I[tid][1]:d} lphi:{data.I[tid][2]:d}\n")
+            # results_file.write(f"    Ps {data.P[tid]}\n")
+            results_file.write(f"    Os {data.O[tid].tolist()}\n")
+            # results_file.write(f'    Popt {data.P[tid][np.argmin(data.O[tid])]}  Oopt {-min(data.O[tid])[0]}  nth {np.argmin(data.O[tid])}\n')
+        results_file.close()            
+        
+    if(TUNER_NAME=='GPTuneBand'):
+        NS = Nloop
+        data = Data(problem)
+        gt = GPTune_MB(problem, computer=computer, NS=Nloop, options=options)
+        (data, stats, data_hist)=gt.MB_LCM(NS = Nloop, Igiven = giventask, Pdefault=Pdefault)
+        print("Tuner: ", TUNER_NAME)
+        print("Sampler class: ", options['sample_class'])
+        print("Model class: ", options['model_class'])
+        # print("stats: ", stats)
+        results_file = open(f"nimrod_ntask{args.ntask}_bandit{args.bmin}-{args.bmax}-{args.eta}_Nloop{args.Nloop}_expid{args.expid}.txt", "a")
+        results_file.write(f"Tuner: {TUNER_NAME}\n")
+        results_file.write(f"stats: {stats}\n")        
+        """ Print all input and parameter samples """
+        for tid in range(NI):
+            print("tid: %d" % (tid))
+            print("    mx:%s my:%s lphi:%s"%(data.I[tid][0],data.I[tid][1],data.I[tid][2]))
+            print("    Ps ", data.P[tid])
+            print("    Os ", data.O[tid].tolist())
+            nth = np.argmin(data.O[tid])
+            Popt = data.P[tid][nth]
+            # find which arm and which sample the optimal param is from
+            for arm in range(len(data_hist.P)):
+                try:
+                    idx = (data_hist.P[arm]).index(Popt)
+                    arm_opt = arm
+                except ValueError:
+                    pass
+            print('    Popt ', Popt, 'Oopt ', min(data.O[tid])[0], 'nth ', nth, 'nth-bandit (s, nth) = ', (arm_opt, idx))
+            results_file.write(f"tid: {tid:d}\n")
+            results_file.write(f"    mx:{data.I[tid][0]:d} my:{data.I[tid][1]:d} lphi:{data.I[tid][2]:d}\n")
+            # results_file.write(f"    Ps {data.P[tid]}\n")
+            results_file.write(f"    Os {data.O[tid].tolist()}\n")
+            # results_file.write(f'    Popt {data.P[tid][np.argmin(data.O[tid])]}  Oopt {-min(data.O[tid])[0]}  nth {np.argmin(data.O[tid])}\n')
+        results_file.close()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-bmin', type=int, default=1, help='budget min')   
+    parser.add_argument('-bmax', type=int, default=1, help='budget max')   
+    parser.add_argument('-eta', type=int, default=2, help='eta')
+    parser.add_argument('-nstepmax', type=int, default=-1, help='maximum number of time steps')   
+    parser.add_argument('-nstepmin', type=int, default=-1, help='minimum number of time steps')   
+    parser.add_argument('-optimization', type=str,default='GPTune',help='Optimization algorithm (opentuner, hpbandster, GPTune)')
+    parser.add_argument('-ntask', type=int, default=1, help='Number of tasks')
+    parser.add_argument('-nrun', type=int, default=-1, help='total application runs')
+    parser.add_argument('-Nloop', type=int, default=1, help='Number of outer loops in multi-armed bandit per task')
+    # parser.add_argument('-sample_class', type=str,default='SampleOpenTURNS',help='Supported sample classes: SampleLHSMDU, SampleOpenTURNS')
+   
+    parser.add_argument('-seed', type=int, default=1, help='random seed')
+    parser.add_argument('-expid', type=str, default='-', help='run id for experiment')
+   
+    args = parser.parse_args()
+    
+    return args   
+
+
+if __name__ == "__main__":
+    main()
---
+++ examples/NIMROD_RCI/nimrod_single_slu3d_MB_RCI.sh
@@ -0,0 +1,236 @@
+#!/bin/bash
+start=`date +%s`
+
+
+# Get nstepmax, nstepmin, Nloop, optimization, expid, seed from command line
+while getopts "a:b:c:d:e:f:" opt
+do
+   case $opt in
+      a ) nstepmax=$OPTARG ;;
+      b ) nstepmin=$OPTARG ;;
+      c ) Nloop=$OPTARG ;;
+      d ) optimization=$OPTARG ;;
+      e ) expid=$OPTARG ;;
+      f ) seed=$OPTARG ;;
+      ? ) echo "unrecognized bash option $opt" ;; # Print helpFunction in case parameter is non-existent
+   esac
+done
+
+
+cd ../../
+export PYTHONPATH=$PYTHONPATH:$PWD/autotune/
+export PYTHONPATH=$PYTHONPATH:$PWD/scikit-optimize/
+export PYTHONPATH=$PYTHONPATH:$PWD/mpi4py/
+export PYTHONPATH=$PYTHONPATH:$PWD/GPTune/
+export PYTHONPATH=$PYTHONPATH:$PWD/GPy/
+export PYTHONWARNINGS=ignore
+
+cd -
+
+# name of your machine, processor model, number of compute nodes, number of cores per compute node, which are defined in .gptune/meta.json
+declare -a machine_info=($(python -c "from gptune import *;
+(machine, processor, nodes, cores)=list(GetMachineConfiguration());
+print(machine, processor, nodes, cores)"))
+machine=${machine_info[0]}
+processor=${machine_info[1]}
+nodes=${machine_info[2]}
+cores=${machine_info[3]}
+
+
+obj=time    # name of the objective defined in the python file
+bmin=1
+bmax=8
+eta=2
+
+database="gptune.db/NIMROD.json"  # the phrase SuperLU_DIST should match the application name defined in .gptune/meta.jason
+rm -rf $database
+
+# start the main loop
+more=1
+while [ $more -eq 1 ]
+do
+
+# call GPTune and ask for the next sample point
+python ./nimrod_single_slu3d_MB_RCI.py -nstepmax $nstepmax -nstepmin $nstepmin -Nloop $Nloop -bmin $bmin -bmax $bmax -eta $eta -optimization $optimization -expid $expid -seed $seed
+
+
+# check whether GPTune needs more data
+idx=$( jq -r --arg v0 $obj '.func_eval | map(.evaluation_result[$v0] == null) | index(true) ' $database )
+if [ $idx = null ]
+then
+more=0
+fi
+
+# if so, call the application code
+while [ ! $idx = null ]; 
+do 
+echo " $idx"    # idx indexes the record that has null objective function values
+# write a large value to the database. This becomes useful in case the application crashes. 
+bigval=1e30
+jq --arg v0 $obj --argjson v1 $idx --argjson v2 $bigval '.func_eval[$v1].evaluation_result[$v0]=$v2' $database > tmp.json && mv tmp.json $database
+
+declare -a input_para=($( jq -r --argjson v1 $idx '.func_eval[$v1].task_parameter' $database | jq -r '.[]'))
+declare -a tuning_para=($( jq -r --argjson v1 $idx '.func_eval[$v1].tuning_parameter' $database | jq -r '.[]'))
+
+
+
+#############################################################################
+#############################################################################
+# Modify the following according to your application !!! 
+
+# get the task input parameters, the parameters should follow the sequence of definition in the python file
+if [ $optimization = 'GPTune' ];then
+    mx=${input_para[0]}
+    my=${input_para[1]}
+    lphi=${input_para[2]}
+    nstep=$nstepmax
+elif [ $optimization = 'GPTuneBand' ];then
+    budget=${input_para[0]}
+    mx=${input_para[1]}
+    my=${input_para[2]}
+    lphi=${input_para[3]}  
+
+    nstep=$(python -c "b=$budget;bmin=$bmin;bmax=$bmax;nmin=$nstepmin;nmax=$nstepmax
+k1 = (nmax-nmin)/(bmax-bmin)
+b1 = nmin - k1
+assert k1 * bmax + b1 == nmax
+result = int(k1 * b + b1)
+print(result)")
+
+fi
+
+# get the tuning parameters, the parameters should follow the sequence of definition in the python file
+NSUP=${tuning_para[0]}
+NREL=${tuning_para[1]}
+nbx=${tuning_para[2]}
+nby=${tuning_para[3]}
+npz=${tuning_para[4]}
+
+# call the application
+NTH=1
+export OMP_NUM_THREADS=$NTH # flat MPI
+export NREL=$NREL
+export NSUP=$NSUP
+
+nproc=$(($nodes*$cores))
+
+if [[ $ModuleEnv == *"openmpi"* ]]; then
+############ openmpi
+    BINDIR="/project/projectdirs/m2957/liuyangz/my_research/nimrod/nimdevel_superlu_v7/build_haswell_gnu_openmpi/bin"
+    RUNDIR="/project/projectdirs/m2957/liuyangz/my_research/nimrod/nimrod_input"
+elif [[ $ModuleEnv == *"mpich"* ]]; then
+############ mpich
+if [[ $ModuleEnv == *"haswell"* ]]; then
+    BINDIR="/project/projectdirs/m2957/liuyangz/my_research/nimrod/nimdevel_superlu_v7/build_haswell_gnu_craympich/bin"
+    RUNDIR="/project/projectdirs/m2957/liuyangz/my_research/nimrod/nimrod_input_craympich"
+elif [[ $ModuleEnv == *"knl"* ]]; then
+    BINDIR="/project/projectdirs/m2957/liuyangz/my_research/nimrod/nimdevel_superlu_v7/build_knl_gnu_craympich/bin"
+    RUNDIR="/project/projectdirs/m2957/liuyangz/my_research/nimrod/nimrod_input_craympich"
+fi
+fi
+
+cp $RUNDIR/nimrod.in ./nimrod_template.in
+cp $RUNDIR/fluxgrid.in .
+cp $RUNDIR/g163518.03130 .
+cp $RUNDIR/p163518.03130 .
+cp $RUNDIR/nimset .
+cp $BINDIR/nimrod .
+
+
+COLPERM='4'
+ROWPERM='1'
+
+
+tmp=$(python -c "import numpy as np
+fin = open(\"./nimrod_template.in\",\"rt\")
+fout = open(\"./nimrod.in\",\"wt\")
+for line in fin:
+    #read replace the string and write to output file
+    if(line.find(\"iopts(3)\")!=-1):
+        fout.write(\"iopts(3)= %s\n\"%($ROWPERM))
+    elif(line.find(\"iopts(4)\")!=-1):
+        fout.write(\"iopts(4)= %s\n\"%($COLPERM))  
+    elif(line.find(\"iopts(6)\")!=-1):
+        fout.write(\"iopts(6)= %s\n\"%(2**$npz)) 
+    elif(line.find(\"lphi\")!=-1):
+        fout.write(\"lphi= %s\n\"%($lphi))    
+    elif(line.find(\"nlayers\")!=-1):
+        fout.write(\"nlayers= %s\n\"%(int(np.floor(2**$lphi/3.0)+1)))  	
+    elif(line.find(\"mx\")!=-1):
+        fout.write(\"mx= %s\n\"%(2**$mx))
+    elif(line.find(\"nstep\")!=-1):
+        fout.write(\"nstep= %s\n\"%($nstep))  			  
+    elif(line.find(\"my\")!=-1):
+        fout.write(\"my= %s\n\"%(2**$my))   
+    elif(line.find(\"nxbl\")!=-1):
+        fout.write(\"nxbl= %s\n\"%(int(2**$mx/2**$nbx)))  
+    elif(line.find(\"nybl\")!=-1):
+        fout.write(\"nybl= %s\n\"%(int(2**$my/2**$nby)))  									  						        
+    else:
+        fout.write(line)
+#close input and output files
+fin.close()
+fout.close()")
+
+
+    nproc=$(python -c "import numpy as np    
+nlayers=int(np.floor(2**$lphi/3.0)+1)
+nprocmax=$nodes*$cores
+nproc = int(nprocmax/nlayers)*nlayers
+if(nprocmax<nlayers):
+    print(0)
+    raise Exception(\"nprocmax<nlayers\")
+if(nproc>int(2**$mx/2**$nbx)*int(2**$my/2**$nby)*int(np.floor(2**$lphi/3.0)+1)): # nproc <= nlayers*nxbl*nybl
+    nproc = int(2**$mx/2**$nbx)*int(2**$my/2**$nby)*int(np.floor(2**$lphi/3.0)+1) 
+print(nproc) ")
+
+
+logfile=NIMROD_mx${mx}_my${my}_lphi${lphi}_nstep${nstep}_NSUP${NSUP}_NREL${NREL}_nbx${nbx}_nby${nby}_nproc${nproc}_omp${OMP_NUM_THREADS}.log
+
+
+
+if [[ $ModuleEnv == *"openmpi"* ]]; then
+############ openmpi
+    echo "mpirun --mca btl self,tcp,vader --allow-run-as-root -n $nproc ./nimrod"
+    ./nimset
+    mpirun --mca btl self,tcp,vader -N $cores --bind-to core --allow-run-as-root -n $nproc ./nimrod | tee $logfile
+elif [[ $ModuleEnv == *"craympich"* ]]; then
+############ craympich
+    echo "srun -n $nproc ./nimrod"
+    ./nimset
+    THREADS_PER_RANK=`expr $NTH \* 2`
+    srun -n $nproc -N $nodes -c $THREADS_PER_RANK --cpu_bind=cores ./nimrod | tee $logfile
+fi
+
+
+# get the result (for this example: search the runlog) egrep is needed for scientific notation
+declare -a arr=($(grep 'Loop  time' $logfile | egrep -o "[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?."))
+result=${arr[0]}
+# result=1
+
+# result=1
+echo "nimrod time: mx: $mx, my: $my, lphi: $lphi, nstep: $nstep, NSUP: $NSUP, NREL: $NREL, nbx: $nbx, nby: $nby, result: $result"
+
+
+# write the data back to the database file
+jq --arg v0 $obj --argjson v1 $idx --argjson v2 $result '.func_eval[$v1].evaluation_result[$v0]=$v2' $database > tmp.json && mv tmp.json $database
+idx=$( jq -r --arg v0 $obj '.func_eval | map(.evaluation_result[$v0] == null) | index(true) ' $database )
+
+#############################################################################
+#############################################################################
+
+
+
+done
+done
+
+end=`date +%s`
+
+runtime=$((end-start))
+echo "Total tuning time: $runtime"
+
+if [ $optimization = 'GPTuneBand' ];then
+ntask=1
+expname=nimrod_ntask${ntask}_bandit${bmin}-${bmax}-${eta}_Nloop${Nloop}
+python parse_GPTuneBand_db.py -ntask ${ntask} -save_path ${expname}_expid${expid} -database "./gptune.db/NIMROD.json" -r "time"
+fi
