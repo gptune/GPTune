a58048163a24d877f2be5cc9cdcc164dc2d4b385
Younghyun Cho <yhcho614@gmail.com>
2022-06-30 01:26:11 -0700

    Updated uncertainty computation in weighted sum (naive sum and regression sum)

--- GPTune/search.py
+++ GPTune/search.py
@@ -286,7 +286,7 @@ class SurrogateProblem(object):
                         model_transfer = self.models_transfer[i]
                         ret = model_transfer(point)
                         mu_transfer += 1.0/len(self.models_transfer)*ret[self.problem.OS[o].name][0][0]
-                        var_transfer *= math.pow(ret[self.problem.OS[o].name+"_var"][0][0], num_models_transfer)
+                        var_transfer *= math.pow(max(1e-18, ret[self.problem.OS[o].name+"_var"][0][0]), num_models_transfer)
                     var = max(1e-18, var_transfer)
                     EI.append(1.0/mu_transfer)
                 elif self.options['TLA_method'] == 'Regression':
@@ -311,7 +311,7 @@ class SurrogateProblem(object):
                         model_transfer = self.models_transfer[i]
                         ret = model_transfer(point)
                         mu_transfer += self.models_weights[i+1]*ret[self.problem.OS[o].name][0][0]
-                        var_transfer *= math.pow(ret[self.problem.OS[o].name+"_var"][0][0], self.models_weights[i+1])
+                        var_transfer *= math.pow(max(1e-18, ret[self.problem.OS[o].name+"_var"][0][0]), self.models_weights[i+1])
                     mu = self.models_weights[0]*mu[0][0] + mu_transfer
                     var = max(1e-18, self.models_weights[0]*var[0][0] * var_transfer)
                     std = np.sqrt(var)
